d4d9f61d915648007f8bc8f300fefb92305e082f
CorbanSy <corbansy10@gmail.com>
2024-06-27 23:22:54 -0700

    Updated the serializers for the outfit rating feature

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import FashionItem, UserPreference
+from .models import FashionItem, UserPreference, Outfit, OutfitRecommendation
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,3 +22,14 @@ class UserPreferencesSerializer(serializers.ModelSerializer):
         model = UserPreference
         fields = ["id", "user", "preferred_styles", "preferred_colors"]
         extra_kwargs = {"user": {"read_only":True}}
+
+class OutfitSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Outfit
+        fields = ["id", "user", "image", "rating"]
+        extra_kwargs = {"user": {"read_only": True}}
+
+class OutfitRecommendationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OutfitRecommendation
+        fields = "__all__"
