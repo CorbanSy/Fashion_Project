2544cf1e0078bfe037ca473fed4d0fd2a077fb33
CorbanSy <corbansy10@gmail.com>
2024-06-27 23:34:30 -0700

    Updated the urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import FashionItemListCreate, UserPreferenceDetail
+from .views import FashionItemListCreate, UserPreferenceDetail, OutfitCreateView, OutfitRecommendationView
 
 urlpatterns = [
     path("fashion-items/", FashionItemListCreate.as_view(), name="fashion-item-list"),
     path("user/preferences/", UserPreferenceDetail.as_view(), name="user-preferences"),
+    path("outfits/", OutfitCreateView.as_view(), name="create-outfit"),
+    path("outfits/<int:outfit_id>/recommendations/", OutfitRecommendationView.as_view(), name="outfit-recommendations"),
 ]
