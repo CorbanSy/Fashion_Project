7752a3c93ec200ee833cc679e85e7a92372f1b99
CorbanSy <corbansy10@gmail.com>
2024-06-28 22:18:53 -0700

    added virtual closet frontend page

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import FashionItemListCreate, UserPreferenceDetail, OutfitCreateView, OutfitRecommendationView
+from .views import FashionItemListCreate, UserPreferenceDetail, OutfitCreateView, OutfitRecommendationView, VirtualClostestView
 
 urlpatterns = [
     path("fashion-items/", FashionItemListCreate.as_view(), name="fashion-item-list"),
     path("user/preferences/", UserPreferenceDetail.as_view(), name="user-preferences"),
     path("outfits/", OutfitCreateView.as_view(), name="create-outfit"),
     path("outfits/<int:outfit_id>/recommendations/", OutfitRecommendationView.as_view(), name="outfit-recommendations"),
+    path("virtual-closet/", VirtualClostestView.as_view(), name="virtual-closet"),
 ]
---
+++ frontend/src/pages/VirtualCloset.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import api from ".../api";
+import "../styles/VirtualCloset.css";
+
+function VirtualCloset(){
+    const [closetItems, setClosetItems] = useState([]);
+
+    useEffect(() => {
+        getClosetItems();
+    }, []);
+
+    const getClosetItems = () => {
+        api
+            .get("/api/virtual-closet/")
+            .then((res) => res.data)
+            .then((data) => {
+                setClosetItems(data);
+            })
+            .catch((err) => alert(err));
+    };
+
+    return (
+        <div>
+            <h2>Your Virtual Closet</h2>
+            <div className="closet-item-list">
+                {closetItems.map((item) => (
+                    <div key={item.id} className="closet-item">
+                        <img src={item.item_name} alt={item.item_name} />
+                        <h3>{item.item_name}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default VirtualCloset;
