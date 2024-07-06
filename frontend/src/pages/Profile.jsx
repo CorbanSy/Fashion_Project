715b2a9e15dd98ec2e11e72e4a0b28c0e0bfab1a
CorbanSy <corbansy10@gmail.com>
2024-07-06 00:52:13 -0700

    added more to the profile page like color and body measurements

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import api from '../api';
 import '../styles/Profile.css';
+import measurementImage from '../assets/measurement_image.webp'; // Adjust the path to where your image is stored
 
 const stylesOptions = [
     '00s', '20s', '30s', '40s', '50s', '60s', '70s', '80s', '90s',
@@ -10,6 +11,35 @@ const stylesOptions = [
     'Glamorous', 'Goth', 'Granola', 'Grunge', 'Hipster', 'Kooky', 'Lagenlook', 'Masculine', 'Military',
     'Minimalist', 'Modest', 'Prairie', 'Preppy', 'Punk', 'Racy', 'Rocker', 'Romantic', 'Skateboard',
     'Sporty', 'Street', 'Toddler', 'Traditional', 'Vintage'
+];
+
+const colorsOptions = [
+    { name: 'Red', color: '#FF0000' },
+    { name: 'Crimson', color: '#DC143C' },
+    { name: 'Maroon', color: '#800000' },
+    { name: 'Orange', color: '#FFA500' },
+    { name: 'Coral', color: '#FF7F50' },
+    { name: 'Salmon', color: '#FA8072' },
+    { name: 'Yellow', color: '#FFFF00' },
+    { name: 'Gold', color: '#FFD700' },
+    { name: 'Green', color: '#008000' },
+    { name: 'Lime', color: '#00FF00' },
+    { name: 'Olive', color: '#808000' },
+    { name: 'Blue', color: '#0000FF' },
+    { name: 'Navy', color: '#000080' },
+    { name: 'Sky Blue', color: '#87CEEB' },
+    { name: 'Purple', color: '#800080' },
+    { name: 'Lavender', color: '#E6E6FA' },
+    { name: 'Magenta', color: '#FF00FF' },
+    { name: 'Pink', color: '#FFC0CB' },
+    { name: 'Hot Pink', color: '#FF69B4' },
+    { name: 'Brown', color: '#A52A2A' },
+    { name: 'Chocolate', color: '#D2691E' },
+    { name: 'Tan', color: '#D2B48C' },
+    { name: 'Gray', color: '#808080' },
+    { name: 'Silver', color: '#C0C0C0' },
+    { name: 'Black', color: '#000000' },
+    { name: 'White', color: '#FFFFFF' }
 ];
 
 const bodyMeasurementsOptions = [
@@ -22,7 +52,7 @@ function Profile() {
     const [profileData, setProfileData] = useState({
         profile_picture: null,
         bio: '',
-        favorite_colors: '',
+        favorite_colors: [],
         favorite_styles: [],
         body_measurements: {}
     });
@@ -43,24 +73,22 @@ function Profile() {
                 [name]: files[0]
             });
             setProfilePicturePreview(URL.createObjectURL(files[0]));
-        } else if (name === "favorite_styles") {
+        } else if (name === "favorite_styles" || name === "favorite_colors") {
             const values = Array.from(selectedOptions, option => option.value);
             setProfileData({
                 ...profileData,
                 [name]: values
             });
-        } else if (bodyMeasurementsOptions.includes(name)) {
-            const measurements = value.split(',').reduce((acc, curr) => {
-                const [key, val] = curr.split(':');
-                acc[key.trim()] = val.trim();
-                return acc;
-            }, {});
-            setProfileData({
+        } else if (name.startsWith("measurement_")) {
+            const measurementName = name.replace("measurement_", "");
+            setProfileData(prevState => ({
                 ...profileData,
-                [name]: value
-            });
-        }
-        else {
+                body_measurements: {
+                    ...prevState.body_measurements,
+                    [measurementName]: value
+                }
+            }));
+        } else {
             setProfileData({
                 ...profileData,
                 [name]: value
@@ -68,11 +96,21 @@ function Profile() {
         }
     };
 
+    const handleMeasurementChange = (measurement, change) => {
+        setProfileData(prevState => ({
+            ...prevState,
+            body_measurements: {
+                ...prevState.body_measurements,
+                [measurement]: (parseInt(prevState.body_measurements[measurement] || 0) + change).toString()
+            }
+        }));
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData();
         for (let key in profileData) {
-            if (key === "favorite_styles" || key === "body_measurements") {
+            if (key === "favorite_styles" || key === "favorite_colors" || key === "body_measurements") {
                 formData.append(key, JSON.stringify(profileData[key]));
             } else {
                 formData.append(key, profileData[key]);
@@ -83,14 +121,28 @@ function Profile() {
             .catch(error => console.error('Error updating profile:', error));
     };
 
-    const formatBodyMeasurements = (measurements) => {
-        return bodyMeasurementsOptions.map(option => `${option}: ${measurements[option] || ''}`).join(', ');
-    }
+    const formatBodyMeasurements = () => {
+        return bodyMeasurementsOptions.map((measurement, index) => (
+            <div key={index} className='measurement-input-group'>
+                <label>{measurement}</label>
+                <div className='measurement-input'>
+                    <button type='button' onClick={() => handleMeasurementChange(measurement, -1)}>-</button>
+                    <input
+                        type='number'
+                        name={`measurement_${measurement}`}
+                        value={profileData.body_measurements[measurement] || ''}
+                        onChange={handleChange}
+                    />
+                    <button type='button' onClick={() => handleMeasurementChange(measurement, 1)}>+</button>
+                </div>
+            </div>
+        ));
+    };
 
     return (
         <div className="profile-container">
             <h2>Profile</h2>
-            <form onSubmit={handleSubmit} className="profile-form">
+            <form onSubmit={handleSubmit} className="profile-form-container">
                 <div className="form-group">
                     <label>Profile Picture</label>
                     <input type="file" name="profile_picture" onChange={handleChange} />
@@ -102,7 +154,14 @@ function Profile() {
                 </div>
                 <div className="form-group">
                     <label>Favorite Colors</label>
-                    <input type="text" name="favorite_colors" value={profileData.favorite_colors} onChange={handleChange} />
+                    <select name="favorite_colors" value={profileData.favorite_colors} onChange={handleChange} multiple>
+                        {colorsOptions.map((color, index) => (
+                            <option key={index} value={color.name}>
+                                <span className="color-option" style={{ backgroundColor: color.color }}></span>
+                                {color.name}
+                            </option>
+                        ))}
+                    </select>
                 </div>
                 <div className="form-group">
                     <label>Favorite Styles</label>
@@ -112,13 +171,16 @@ function Profile() {
                         ))}
                     </select>
                 </div>
-                <div className="form-group">
-                    <label>Body Measurements</label>
-                    <textarea
-                        name="body_measurements"
-                        value={formatBodyMeasurements(profileData.body_measurements)}
-                        onChange={handleChange}
-                    ></textarea>
+                <div className="body-measurements-container">
+                    <div className="measurements-box">
+                        <div className="form-group">
+                            <label>Body Measurements</label>
+                            <div className="measurements-input-container">
+                                {formatBodyMeasurements()}
+                            </div>
+                        </div>
+                    </div>
+                    <img src={measurementImage} alt="Body Measurements Guide" className="measurement-image" />
                 </div>
                 <button type="submit">Save Profile</button>
             </form>
